3ea20230a301e0aff946646226a95e6aa775bd5f
ben3dct <yennexl@gmail.com>
2023-02-24 11:37:25 +0100

    add authentication

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+/** @format */
+
+import "./App.css";
+
+import * as React from "react";
+
+import { Amplify, Auth, Hub } from "aws-amplify";
+import { CognitoHostedUIIdentityProvider } from "@aws-amplify/auth";
+
+import awsconfig from "./aws-exports";
+Amplify.configure(awsconfig);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [uset, setUser] = React.useState(null);
+	React.useEffect(() => {
+		const unsubscribe = Hub.listen("auth", ({ payload: { event, data } }) => {
+			// eslint-disable-next-line default-case
+			switch (event) {
+				case "signIn":
+					setUser(data);
+					break;
+				case "signOut":
+					setUser(data);
+					break;
+			}
+		});
+
+		Auth.currentAuthenticatedUser()
+			.then((currentUser) => setUser(currentUser))
+			.catch(() => console.log("not signed in"));
+
+		return unsubscribe;
+	}, []);
+
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<button onClick={() => Auth.federatedSignIn({ provider: CognitoHostedUIIdentityProvider.Google })}>
+					sign in with google
+				</button>
+			</header>
+		</div>
+	);
 }
 
 export default App;
