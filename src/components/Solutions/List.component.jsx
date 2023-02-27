3e135f7414f5a5bcad46bea9d4deee32097c2883
ben3dct <yennexl@gmail.com>
2023-02-27 11:15:28 +0100

    table colums

--- src/components/Solutions/List.component.jsx
+++ src/components/Solutions/List.component.jsx
@@ -2,21 +2,24 @@
 
 import * as React from "react";
 import "./List.styles.scss";
-import { Row } from "./Table.component";
+import { Row, Columns } from "./Table.component";
 const array = [{ id: 1 }, { id: 2 }, { id: 3 }];
 
 export const List = (props) => {
 	return (
 		<div className='solution-list-container'>
-			{array.map(({ id }) => {
-				console.log(id);
-				return (
-					<Row
-						key={id}
-						id={id}
-					/>
-				);
-			})}
+			<Columns />
+			<div className='item-container'>
+				{array.map(({ id }) => {
+					console.log(id);
+					return (
+						<Row
+							key={id}
+							id={id}
+						/>
+					);
+				})}
+			</div>
 		</div>
 	);
 };
--- src/components/Solutions/Table.component.jsx
+++ src/components/Solutions/Table.component.jsx
@@ -14,7 +14,32 @@ export const Row = ({ id }) => {
 				<div className='category'>abc</div>
 			</div>
 			<div className='row-group-lg'>
-				<div className='description'>desc</div>
+				<div className='description'>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+					dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+					ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+					fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+					mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+					incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+					laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+					velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt
+					in culpa qui officia deserunt mollit anim id est laborum.
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export const Columns = () => {
+	return (
+		<div className='solution-list-columns'>
+			<div className='row-group-md'>
+				<div className='title'>Title</div>
+				<div className='author'>Author</div>
+				<div className='category'>Category</div>
+			</div>
+			<div className='row-group-lg'>
+				<div className='description'>Description</div>
 			</div>
 		</div>
 	);
